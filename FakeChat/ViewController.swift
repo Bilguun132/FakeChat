3809b1983f5d20fc413bbe6e51a91bbfee8eac35
Bilguun <bilguun132@gmail.com>
2019-03-23 11:34:42 +0800

    Initial Commit

---
+++ FakeChat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FakeChat
+//
+//  Created by Bilguun Batbold on 23/3/19.
+//  Copyright © 2019 Bilguun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
